707c8d452f110d5fba27a5e4fbaf2c99d3369b3a
Rashmi0986 <56731498+Rashmi0986@users.noreply.github.com>
2020-11-11 18:48:09 +0530

    Create logparser.py

---
+++ logparser.py
@@ -0,0 +1,40 @@
+import os 
+import re
+
+
+regex = r'''Exception'''
+for dirPath,dirNames,files in os.walk('IPV4_ADDR/'):
+    curDir = os.path.join(dirPath) # needed to find in the curreDirectory or else throws file not found exception 
+    for fileName in files:
+        curFile = os.path.join(curDir , fileName)
+        if os.stat(curFile).st_size != 0: # if current file is not empty
+            result = []
+            with open(curFile) as f:
+                 for (i,line) in enumerate(f):
+                     m = re.search(regex, line)
+                     if m:
+                        print (f' {m.group()} found in {fileName} located at path {curFile} at line num {m.span()[0]}')
+
+
+
+"""
+Output :
+
+ RASHMIBSs-MacBook-Pro:RealPython_All_files rashmibs$ python3 logParser.py 
+ Exception found in logger.txt located at path IPV4_ADDR/logger.txt at line num 12
+ Exception found in logger.txt located at path IPV4_ADDR/logger.txt at line num 11
+ Exception found in logger.txt located at path IPV4_ADDR/logger.txt at line num 12
+ Exception found in logger.txt located at path IPV4_ADDR/logger.txt at line num 7
+ Exception found in logger.txt located at path IPV4_ADDR/logger.txt at line num 22
+ Exception found in logger.txt located at path IPV4_ADDR/logger.txt at line num 7
+ Exception found in file_6.txt located at path IPV4_ADDR/Dir_2/file_6.txt at line num 12
+ Exception found in file_6.txt located at path IPV4_ADDR/Dir_2/file_6.txt at line num 2
+ Exception found in file7.txt located at path IPV4_ADDR/Dir_2/file7.txt at line num 11
+ Exception found in file8.txt located at path IPV4_ADDR/Dir_2/file8.txt at line num 11
+ Exception found in file_4.txt located at path IPV4_ADDR/Dir_1/file_4.txt at line num 21
+ Exception found in file_4.txt located at path IPV4_ADDR/Dir_1/file_4.txt at line num 13
+ Exception found in file_3.txt located at path IPV4_ADDR/Dir_1/file_3.txt at line num 10
+ Exception found in file_3.txt located at path IPV4_ADDR/Dir_1/file_3.txt at line num 12
+ Exception found in file_3.txt located at path IPV4_ADDR/Dir_1/file_3.txt at line num 11
+ 
+ """"
