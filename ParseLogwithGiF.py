b71e15a1ce9f77ecd4b95c046529ba5ec94166cd
Rashmi BS <56731498+Rashmi0986@users.noreply.github.com>
2024-07-16 19:24:07 +0530

    Create ParseLogwithGiF.py

---
+++ ParseLogwithGiF.py
@@ -0,0 +1,9 @@
+"""
+Given the log file containing the response 
+Create an output file prefixed with gif_output.txt
+Process the line if and only if 
+the line has  GET httpverb , 200 Status code  and
+ should have the file name then process it and consider he gif file only and store it in the gif_outfile.txt
+
+And return the same 
+"""
