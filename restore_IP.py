fb23adb13448d8cf0939c8b4856e4738fa539b57
Rashmi BS <56731498+Rashmi0986@users.noreply.github.com>
2025-08-20 19:48:02 +0530

    Update restore_IP.py
    Updated this with concise code .

--- restore_IP.py
+++ restore_IP.py
@@ -15,29 +15,21 @@ class Solution:
 
 
 #restore_Ip with backtracking 
-def restoreIpAddressesAlt(s):
-    """
-    Alternative implementation with slightly different structure
-    """
-    def backtrack(start, path, result):
-        # If we have 4 parts and used all characters
+def restoreIpAddresses(s):
+    res = []
+    def backtrack(start, path):
         if len(path) == 4:
             if start == len(s):
-                result.append('.'.join(path))
+                res.append(".".join(path))
             return
-        
-        # Try each possible length for next part
-        for i in range(start, min(start + 3, len(s))):
-            part = s[start:i + 1]
-            
-            # Validate the part
-            if (len(part) == 1 or 
-                (len(part) > 1 and part[0] != '0')) and int(part) <= 255:
-                
-                path.append(part)
-                backtrack(i + 1, path, result)
-                path.pop()
-    
-    result = []
-    backtrack(0, [], result)
-    return result
+        for l in range(1, 4):
+            if start + l > len(s):
+                break
+            part = s[start:start+l]
+            if (part.startswith("0") and len(part) > 1) or int(part) > 255:
+                continue
+            backtrack(start + l, path + [part])
+    backtrack(0, [])
+    return res
+
+
